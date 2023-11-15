4fe9f5ff89bd59bbf731e37f337599dd48b866e6
cahudisa <cahudisa1999@gmail.com>
2023-11-15 10:09:20 -0500

    final

--- Practica3/ook_epy_block_0.py
+++ Practica3/ook_epy_block_0.py
@@ -2,8 +2,24 @@ import numpy as np
 from gnuradio import gr
 import math
 
-class blk(gr.sync_block):  
-    """This block is a RF VCO and works as following: ….."""
+class e_RF_VCO_ff(gr.sync_block):  
+    """
+    This block is an RF Voltage-Controlled Oscillator (RF VCO).
+    
+    Parameters:
+    - fc (float): Center frequency of the VCO.
+    - samp_rate (float): Sampling rate.
+    
+    The block takes in two input signals, A (Amplitude) and Q (Quadrature),
+    and produces an RF-modulated output signal.
+
+    The block modulates the amplitude (A) with a cosine waveform, where the
+    frequency is determined by the center frequency (fc). The phase of the cosine
+    is controlled by the quadrature (Q).
+
+    The output signal is given by: y = A * cos(2 * pi * fc * n / samp_rate + Q),
+    where n is the sample index.
+    """
 
     def __init__(self, fc=128000, samp_rate=320000):  
         gr.sync_block.__init__(
@@ -14,16 +30,25 @@ class blk(gr.sync_block):
         )
         self.fc = fc
         self.samp_rate = samp_rate
-        self.n_m=0
+        self.n_m = 0
 
     def work(self, input_items, output_items):
-        A=input_items[0]
-        Q=input_items[1]
-        y=output_items[0]
-        N=len(A)
-        n=np.linspace(self.n_m,self.n_m+N-1,N)
+        # Extract input signals
+        A = input_items[0]
+        Q = input_items[1]
+        
+        # Extract output signal
+        y = output_items[0]
+        
+        # Number of samples
+        N = len(A)
+        
+        # Sample index
+        n = np.linspace(self.n_m, self.n_m + N - 1, N)
         self.n_m += N
-        y[:]=A*np.cos(2*math.pi*self.fc*n/self.samp_rate+Q)
+        
+        # Perform modulation: y = A * cos(2 * pi * fc * n / samp_rate + Q)
+        y[:] = A * np.cos(2 * math.pi * self.fc * n / self.samp_rate + Q)
+        
         return len(output_items[0])
-
 
--- Practica3/ook_epy_block_0_0.py
+++ Practica3/ook_epy_block_0_0.py
@@ -1,11 +1,22 @@
 import numpy as np
 from gnuradio import gr
-import math
 
-class blk(gr.sync_block):  
-    """This block is a CE VCO or baseband VCO and works as following: ….."""
+class e_CE_VCO_fc(gr.sync_block):  
+    """
+    This block is a Complex Envelope Voltage-Controlled Oscillator (CE VCO)
+    and works as follows: It takes in two input signals, A (Amplitude) and Q (Quadrature),
+    and produces a complex output signal y = A * exp(1j * Q).
 
-    def __init__(self,):  
+    Parameters:
+    - A (input_items[0]): Input signal representing the amplitude.
+    - Q (input_items[1]): Input signal representing the quadrature.
+    - y (output_items[0]): Complex output signal.
+
+    The block performs the modulation operation by multiplying the amplitude (A)
+    with the complex exponential of the quadrature (Q), producing the modulated signal.
+    """
+
+    def __init__(self):  
         gr.sync_block.__init__(
             self,
             name='e_CE_VCO_fc',   
@@ -14,9 +25,18 @@ class blk(gr.sync_block):
         )
         
     def work(self, input_items, output_items):
-        A=input_items[0]
-        Q=input_items[1]
-        y=output_items[0]
-        N=len(A)
-        y[:]=A*np.exp(1j*Q)
+        # Extract input signals
+        A = input_items[0]
+        Q = input_items[1]
+        
+        # Extract output signal
+        y = output_items[0]
+        
+        # Number of samples
+        N = len(A)
+        
+        # Perform modulation: y = A * exp(1j * Q)
+        y[:] = A * np.exp(1j * Q)
+        
         return len(output_items[0])
+
